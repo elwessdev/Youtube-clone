9d009f9735023a8dbdd87f0587bec5c7f96b0905
CoderOsama <bms93951@gmail.com>
2024-01-31 13:31:39 +0100

    fix search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -57,7 +57,7 @@ export default function SearchBar(){
                         <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
                     </svg>
                 </Link>
-                {(searchValue.length>3&&searchResults.length>0&&hidePop) && (
+                {(searchResults.length>0&&hidePop) && (
                     <div className='search-auto-complete'>
                         {searchResults.map(result=>(<SearchAutoComplete value={result} hidePope={setHidePop}  />))}
                     </div>
